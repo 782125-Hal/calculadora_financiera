1879cd809a44d44ae577ecad7d253dd14a33a70f
Humberto Alvarado <53151742+782125-Hal@users.noreply.github.com>
2025-06-21 20:09:34 -0700

    Fix loan payment calculation

--- finanzas/tests.py
+++ finanzas/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Prestamo
 
-# Create your tests here.
+
+class PrestamoCalculoTest(TestCase):
+    def test_calcular_cuota_mensual(self):
+        prestamo = Prestamo(monto=1000, plazo=12, tasa_anual=12, frecuencia_pago='mensual')
+        cuota = prestamo.calcular_cuota()
+        self.assertAlmostEqual(cuota, 88.85, places=2)
+
+    def test_calcular_cuota_semanal(self):
+        prestamo = Prestamo(monto=1000, plazo=12, tasa_anual=12, frecuencia_pago='semanal')
+        cuota = prestamo.calcular_cuota()
+        self.assertAlmostEqual(cuota, 20.43, places=2)
